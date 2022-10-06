f1dcfdc11de569001d583d5d0f11866d00908ee6
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-10-07 00:07:21 +0500

    1st initialization of program. Hello User

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Dear, User";
+
+        System.out.println("Hello " + name);
+        System.out.println(name);
+    }
+}
