e3856380aa0fe77976acd5a2acc7e4b61a9980f2
almasmendybayev <almasmendybayev@tengizchevroil.com>
2022-10-07 12:09:07 +0500

    new vars for console output, test arrays creating

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         int userOld = 21;
-        float userHeight = 180.8f;//sm
+        float userHeight = 180.8F;//sm
+        double userWeight = 62.6D;//kg
+
+        Boolean isWorkDay = false;
         String name = "Dear, User";
+        String workDays[] = new String[]{ "Mn", "Tu", "We", "Th", "Fi" };
+        int numbers[] = {1, 2, 3, 4, 5};
 
         System.out.println("user old is " + userOld + ", user height: " + userHeight + ", please input anything");
+        System.out.println(numbers[0] + " " + workDays[0]);
+        System.out.println(numbers[1] + " " + workDays[1]);
+        System.out.println(numbers[2] + " " + workDays[2]);
+        System.out.println(numbers[3] + " " + workDays[3]);
+        System.out.println(numbers[4] + " " + workDays[4]);
     }
 }
